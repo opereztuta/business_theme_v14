03e8f6729a555c05df5ff3ac194d4ed567c28857
Oscar Adolfo Perez Tuta <operez@zoni.edu>
2024-04-10 21:20:31 -0400

    Zoni ERP Theme

--- business_theme_v14/hooks.py
+++ business_theme_v14/hooks.py
@@ -17,7 +17,7 @@ app_include_css = "/assets/business_theme_v14/css/business_theme_v14.css"
 # app_include_js = "/assets/business_theme_v14/js/business_theme_v14.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/business_theme_v14/css/business_theme_v14.css"
+web_include_css = "/assets/business_theme_v14/css/business_theme_v14.css"
 # web_include_js = "/assets/business_theme_v14/js/business_theme_v14.js"
 
 # include custom scss in every website theme (without file extension ".scss")
